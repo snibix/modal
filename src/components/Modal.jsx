42a87be84a22308aeb76ed7a0cb94880343e3d13
Damien <damien.dev2022@gmail.com>
2024-11-27 13:27:46 +0100

    update read me + jsdoc modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,30 @@
 import PropTypes from "prop-types";
 import React, { useEffect } from "react";
 
+/**
+ * Modal component to display customizable modals in your application.
+ *
+ * @param {Object} props - The props object.
+ * @param {boolean} props.isOpen - Determines whether the modal is open or closed.
+ * @param {React.ReactNode} props.children - The content to display inside the modal.
+ * @param {function} props.close - Function to close the modal, triggered on escape key or backdrop click.
+ * @param {...Object} args - Additional props spread to the modal container.
+ *
+ * @example
+ * // Basic usage:
+ * <Modal isOpen={true} close={() => console.log('Closed')}>
+ *   <h1>Hello, Modal!</h1>
+ * </Modal>
+ *
+ * @example
+ * // Custom styling with a className:
+ * <Modal isOpen={true} close={() => console.log('Closed')} className="custom-modal">
+ *   <div>
+ *     <h2>Custom Modal</h2>
+ *     <p>This is a custom modal.</p>
+ *   </div>
+ * </Modal>
+ */
 function Modal({ isOpen, children, close, ...args }) {
   useEffect(() => {
     const handleEscapeKey = (e) => {
@@ -36,9 +60,9 @@ function Modal({ isOpen, children, close, ...args }) {
 }
 
 Modal.propTypes = {
-  isOpen: PropTypes.bool,
-  children: PropTypes.any,
-  close: PropTypes.func.isRequired,
+  isOpen: PropTypes.bool, // Whether the modal is open or closed
+  children: PropTypes.any, // The content displayed inside the modal
+  close: PropTypes.func.isRequired, // Function to close the modal
 };
 
 export default Modal;
